97b0237cb67b60c0043f788b2fd60bb4ed098aaf
olivier nerot <olivier.nerot@meetsys.com>
2022-09-15 13:50:26 +0200

    +dag

--- index.js
+++ index.js
@@ -59,36 +59,56 @@ async function main()
     console.log( 'peer:disconnect', remotePeer.toString());
   });
 
-  await ipfs.pubsub.subscribe('mypubsub', async (msg) => {
-    console.log('got message : ', msg.from.toString(), uint8ArrayToString(msg.data));
+  await ipfs.pubsub.subscribe('ipfsfilemsg', async (msg) => {
+    console.log('got file message : ', msg.from.toString(), uint8ArrayToString(msg.data));
 
     const cid = CID.parse(uint8ArrayToString(msg.data));
+
+    //read file
     const chunks = [];
     for await (const chunk of ipfs.cat(cid)) {
       chunks.push(chunk);
     }
     const content = uint8ArrayToString(uint8ArrayConcat(chunks));
     console.log('got file : ', content);
+  });
 
+  await ipfs.pubsub.subscribe('ipfsdagmsg', async (msg) => {
+    console.log('got dag message : %o', msg.from.toString(), msg.data);
+
+    // read dag
+    const cid = CID.parse(uint8ArrayToString(msg.data));
+    const result = await ipfs.dag.get(cid);
+    console.log('got dag : ', result.value);
   });
 
   // 2 subscribe => 2 events
-  // await ipfs.pubsub.subscribe('mypubsub', (msg) => {
+  // await ipfs.pubsub.subscribe('ipfsfilemsg', (msg) => {
   //   console.log('got message : ', msg.from.toString(), uint8ArrayToString(msg.data));
   // });
 
   let nfile = 0;
   setInterval(async () => {
-    const content = myPeerId.toString()+' ipfs file #' + nfile++;
-    const result = await ipfs.add(content);
-    console.log('save ipfs : ', result.cid);
-    const CID =  result.path;
+    // show connected peers
+    const peers = await ipfs.pubsub.peers('ipfsfilemsg');
+    peers.forEach((p)=> console.log('peer : ', p.toString()));
 
-    const peers = await ipfs.pubsub.peers('mypubsub');
-    console.log('pubsub peers : %o', peers.length);
+    // put file content
+    const resfile = await ipfs.add(myPeerId.toString()+' ipfs file #' + nfile);
+    console.log('save ipfs : ', resfile.path);
 
-    const msg = new TextEncoder().encode(CID);
-    await ipfs.pubsub.publish('mypubsub', msg);
+    await ipfs.pubsub.publish('ipfsfilemsg',  new TextEncoder().encode(resfile.path));
+
+    // put dag content
+    const resdag = await ipfs.dag.put({
+      content: myPeerId.toString()+' ipfs dag #' + nfile,
+    }, { storeCodec: 'dag-cbor', hashAlg: 'sha2-256' });
+    console.log('save dag : ', resdag.toString()); 
+      
+    await ipfs.pubsub.publish('ipfsdagmsg', new TextEncoder().encode(resdag.toString()));
+
+    nfile++;
+
   },10000);
 
   console.log(await ipfs.bootstrap.list());
