a76c1a041d87e5a28c2590c1c1406463e21981c1
olivier nerot <olivier.nerot@meetsys.com>
2022-09-15 10:59:39 +0200

    go

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const os = require('os');
 const path = require('path');
 const uint8ArrayToString = require('uint8arrays/to-string').toString;
+const uint8ArrayConcat = require('uint8arrays/concat').concat;
+const CID = require('multiformats/cid');
 
 async function main()
 {
   const { createEd25519PeerId } = await import('@libp2p/peer-id-factory');
   const { PreSharedKeyConnectionProtector } = await import('libp2p/pnet');
+  const { GossipSub } = await import('@chainsafe/libp2p-gossipsub');
   const createIpfs = (await import('ipfs')).create;
 
   const myPeerId = await createEd25519PeerId();
@@ -15,16 +18,30 @@ async function main()
 
   const p2pOptions = {
     peerId: myPeerId,
+    pubsub: new GossipSub({
+      allowPublishToZeroPeers: true,
+      fallbackToFloodsub: true,
+      emitSelf: false,
+      maxInboundStreams: 64,
+      maxOutboundStreams: 128,
+    }),
     connectionProtector: new PreSharedKeyConnectionProtector({
       psk: new Uint8Array(Buffer.from(swarmKey, 'base64')),
     }),
   };
 
+  const bootstrap = [];
+  // const bootstrap = [
+  //   '/ip4/5.51.172.39/tcp/4002/p2p/xxx',
+  //   '/ip4/5.51.172.39/tcp/4002/p2p/xxx',
+  //   '/ip4/5.51.172.39/tcp/4003/ws/p2p/xxx',
+  // ];
+
   ipfs = await createIpfs({
     libp2p: p2pOptions,
     repo: path.join(os.homedir(), '.testipfs'),
     config: {
-      Bootstrap: [],
+      Bootstrap: bootstrap,
     },
   });
 
@@ -42,18 +59,37 @@ async function main()
     console.log( 'peer:disconnect', remotePeer.toString());
   });
 
-  await ipfs.pubsub.subscribe('mypubsub', (msg) => {
+  await ipfs.pubsub.subscribe('mypubsub', async (msg) => {
     console.log('got message : ', msg.from.toString(), uint8ArrayToString(msg.data));
+
+    const cid = CID.parse(uint8ArrayToString(msg.data));
+    const chunks = [];
+    for await (const chunk of ipfs.cat(cid)) {
+      chunks.push(chunk);
+    }
+    const content = uint8ArrayToString(uint8ArrayConcat(chunks));
+    console.log('got file : ', content);
+
   });
 
-  let tosend = 0;
+  // 2 subscribe => 2 events
+  // await ipfs.pubsub.subscribe('mypubsub', (msg) => {
+  //   console.log('got message : ', msg.from.toString(), uint8ArrayToString(msg.data));
+  // });
+
+  let nfile = 0;
   setInterval(async () => {
+    const content = myPeerId.toString()+' ipfs file #' + nfile++;
+    const result = await ipfs.add(content);
+    console.log('save ipfs : ', result.cid);
+    const CID =  result.path;
+
     const peers = await ipfs.pubsub.peers('mypubsub');
     console.log('pubsub peers : %o', peers.length);
 
-    const msg = new TextEncoder().encode(tosend++);
+    const msg = new TextEncoder().encode(CID);
     await ipfs.pubsub.publish('mypubsub', msg);
-  },5000);
+  },10000);
 
   console.log(await ipfs.bootstrap.list());
 }
